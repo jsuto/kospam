70101a7d4f15d53499d4bd94332db37e0897782f
SJ <sj@acts.hu>
2009-01-19 16:42:41 +0100

    code cleanup

--- shdr.h
+++
@@ -1,9 +0,0 @@
-/*
- * shdr.h, 2009.01.04, SJ
- */
-
-static char *weekdays[] = { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
-static char *months[] = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December",
-                             "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
-
-static char *dates[] = { "CET", "UTC", "GMT", "EET" };

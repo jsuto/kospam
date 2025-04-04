ee61e5c27af47788be1c5563a0a53f34f86a8210
jsuto <sj@acts.hu>
2025-04-04 04:21:58 +0200

    More fixes (#16)
    Signed-off-by: Janos SUTO <sj@acts.hu>

--- docker/makecert.sh
+++ docker/makecert.sh
@@ -4,15 +4,14 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-pemfile="server.pem"
-
-SSL_CERT_DATA="/C=US/ST=Denial/L=Springfield/O=Dis/CN=mail.kospam"
-
 make_certificate() {
-   openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 -subj "$SSL_CERT_DATA" -keyout "$pemfile" -out 1.cert -sha256 2>/dev/null
+   pemfile="$1"
+   local subject="$2"
+
+   openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 -subj "$subject" -keyout "$pemfile" -out 1.cert -sha256 2>/dev/null
    cat 1.cert >> "$pemfile"
    rm -f 1.cert
    chmod 640 "$pemfile"
 }
 
-make_certificate
+make_certificate "$1" "$2"

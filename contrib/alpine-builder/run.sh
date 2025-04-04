bf4bcf35fa2cb4221014abf4ed691b6c0e8da9c2
Janos SUTO <sj@acts.hu>
2025-04-04 03:09:36 +0200

    Added alpine builder image to contrib dir
    Signed-off-by: Janos SUTO <sj@acts.hu>

---
+++ contrib/alpine-builder/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -o nounset
+set -o errexit
+
+cd /repo
+git config --global --add safe.directory /repo
+
+./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var
+make clean all install
